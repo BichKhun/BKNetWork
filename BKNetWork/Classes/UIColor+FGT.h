0f56236d045487814bce8f9058641bc503c44a9c
K <yubikun@yubikundeiMac.local>
2021-05-17 17:39:32 +0800

    提交代码

---
+++ BKNetWork/Classes/UIColor+FGT.h
@@ -0,0 +1,7 @@
+
+#import <UIKit/UIKit.h>
+@interface UIColor (FGT)
+
++ (UIColor *)WAWAColorWithHexString:(NSString *)colorStr;
++ (UIColor *)WAWAColorWithHexString:(NSString *)colorStr Alpha:(CGFloat)alpha;
+@end
---
+++ Example/Pods/Target Support Files/BKNetWork/BKNetWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BKNetWork : NSObject
+@end
+@implementation PodsDummy_BKNetWork
+@end
